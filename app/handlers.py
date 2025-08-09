3e64a57f483a8305116114f177c6098f3e3e93fc
mrDem52 <93878095+mrDem52@users.noreply.github.com>
2025-08-09 21:21:42 +0300

    clear all chat - bad idea

--- app/handlers.py
+++ app/handlers.py
@@ -1,28 +1,17 @@
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import CommandStart, Command
-from aiogram import Router, F, Bot
+from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery, FSInputFile, ReplyKeyboardRemove
 import app.keyboards as kb
 
 router = Router()
 
+msg_help = 'Бот создан для начинающих любителей логической электроника'
+
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.reply(f'Привет, {message.from_user.first_name}', reply_markup=kb.main)
-
-
-@router.message(Command("clear"))
-async def cmd_clear(message: Message, bot: Bot) -> None:
-    try:
-        # Все сообщения, начиная с текущего и до первого (message_id = 0)
-        for i in range(message.message_id, 0, -1):
-            await bot.delete_message(message.from_user.id, i)
-    except TelegramBadRequest as ex:
-        # Если сообщение не найдено (уже удалено или не существует),
-        # код ошибки будет "Bad Request: message to delete not found"
-        if ex.message == "Bad Request: message to delete not found":
-            print("Все сообщения удалены")
 
 
 @router.message(F.text == 'Логические вентили')
@@ -38,7 +27,6 @@ async def log_block(message: Message):
     await message.answer("Логические блоки,отличный выбор!")
 
 
-@router.callback_query(F.data == 'more')
-async def more(callback: CallbackQuery):
-    await callback.answer()
-    await callback.message.answer('Привет!')
+@router.message(Command('help'))
+async def cmd_clear(message: Message):
+    await message.answer(msg_help)
